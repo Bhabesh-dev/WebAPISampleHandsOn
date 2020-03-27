f8b7c7681e575665a6186d17529b6e7aa7bb0282
Bhabesh-Dev <Bhabesh.barik77@outlook.com>
2020-03-28 00:17:11 +0530

    Add project files.

---
+++ DataLayer/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPISampleHandsOn.Models;
+
+namespace WebAPISampleHandsOn.DataLayer
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+    }
+}
